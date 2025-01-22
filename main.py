00aa91d6038ecb1cb8d4610a8b4f0029c03b258f
Marwan <maroelsays2013@gmail.com>
2025-01-22 22:43:58 +0200

    added intial code

---
+++ main.py
@@ -0,0 +1,13 @@
+# Random Number Generator Program
+# It is made up of two functions: main_menu() and generate_random_number(),
+# and a while loop that runs the program.
+def main_menu():
+    print("1. Generate a random number")
+    print("2. Exit")
+    return input("Enter your choice: ")
+
+def generate_random_number():
+    pass
+    
+while True:
+    pass
